cbb6fa38c40cdf755d9f5cba3af4c3fb63fdf7a1
TJ Koblentz <tj.koblentz@gmail.com>
2013-06-06 18:12:12 -0600

    Why was this based on github username... wtf

--- manifests/personal.pp
+++ manifests/personal.pp
@@ -1,11 +1,10 @@
-# Private: Includes a user's personal manifest based on their github username
+# Private: Includes a user's personal manifest based on their username
 
 class boxen::personal {
   $manifests         = "${boxen::config::repodir}/modules/people/manifests"
-  $login             = regsubst($::github_login, '-','_')
-  $personal_manifest = "${manifests}/${login}.pp"
+  $personal_manifest = "${manifests}/${user}.pp"
 
   if file_exists($personal_manifest) {
-    include "people::${login}"
+    include "people::${user}"
   }
 }
