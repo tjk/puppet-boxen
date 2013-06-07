ae13eb8bbd10afd3d47de124533474324313d2d0
TJ Koblentz <tj@brightroll.com>
2013-06-06 17:49:31 -0700

    keeping good changes

--- manifests/personal.pp
+++ manifests/personal.pp
@@ -1,8 +1,7 @@
 class boxen::personal {
   $manifests = "${boxen::config::repodir}/modules/people/manifests"
-  $login = regsubst("${::github_login}", "-","_")
-  $personal_manifest = "${manifests}/${login}.pp"
+  $personal_manifest = "${manifests}/${::luser}.pp"
   if file_exists($personal_manifest) {
-    include "people::${login}"
+    include "people::${::luser}"
   }
 }
